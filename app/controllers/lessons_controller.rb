d862f6db1779263d99f2fd92cd7bc0df14bfa4df
Nathan <nathangelliott@gmail.com>
2016-01-21 23:02:47 +0000

    Added requirement for enrollment before viewing course lessons - references Firehose Project, Flixster, Lesson 27

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_lesson, only: :show
+
   def show
   end
 
@@ -6,5 +9,15 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def current_course
+    @current_course = current_lesson.section.course
+  end
+
+  def require_authorized_for_lesson
+    if ! current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: "You must be enrolled in this course to view lessons."
+    end
   end
 end
