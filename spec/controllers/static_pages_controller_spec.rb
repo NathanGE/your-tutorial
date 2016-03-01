4ad26f0a5d565876a1bb294ff6e50868ab46737d
Nathan <nathangelliott@gmail.com>
2016-03-01 14:05:44 +0000

    Added controller specs for static pages

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+  describe 'static_pages#index action' do
+    it 'should successfully show the page' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'static_pages#privacy action' do
+    it 'should successfully show the page' do
+      get :privacy
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
